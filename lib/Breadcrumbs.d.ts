590cb659ea603b98b09344b06bcf29636efdae11
Tsubasa Kondo <kondows95@gmail.com>
2020-05-15 04:34:22 +0900

    Rename tsconfig.prod.json

--- lib/Breadcrumbs.d.ts
+++ lib/Breadcrumbs.d.ts
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from '@material-ui/core';
 import { RouteComponentProps } from 'react-router-dom';
 export declare type Link = {
     intlID: string;
@@ -9,5 +8,5 @@ declare type Props = {
     links: Link[];
     fontSize?: number | string;
 } & RouteComponentProps;
-declare const _default: React.ComponentClass<Pick<Props, "links" | "fontSize">, any> & import("react-router").WithRouterStatics<React.FC<Props>>;
+declare const _default: React.ComponentClass<Pick<Props, "fontSize" | "links">, any> & import("react-router").WithRouterStatics<React.FC<Props>>;
 export default _default;
